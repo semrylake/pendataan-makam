a80d8daa9847fe1bd697b58772cce622fce599a0
semrylake <jufentosemry@gmail.com>
2022-11-02 21:37:25 +0700

    change database name and add migration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '56964',
-        'database' => 'sikamci',
+        'username' => 'semry',
+        'password' => '12345',
+        'database' => 'risalbutik',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
